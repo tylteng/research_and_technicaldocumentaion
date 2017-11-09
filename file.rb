115fdcb97361d24b1a19aced37b2364939ae7c15
tylteng <teng.ty4@gmail.com>
2017-11-09 15:47:08 -0500

    completed file documentation

---
+++ file.rb
@@ -0,0 +1,12 @@
+# File
+
+  # exist? - this method is called upon by typing in "File.exist?(x)"
+  # =>  and you plug the file into the parenthesis
+
+p File.exist?('file.rb')
+
+
+  # extname - this method gives you the extension of the file you're
+    # => looking at
+
+p File.extname("hash.rb")
